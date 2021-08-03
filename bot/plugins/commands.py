c53f879ba72ee0d350e6e74469e9eb5cf88ca838
Aswankannan <88327644+Aswankannan@users.noreply.github.com>
2021-08-03 06:39:02 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Developers', url="https://t.me/Cybermediaworks"
                                 )
                         ]
                     ]
@@ -47,12 +47,11 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
-        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
+        InlineKeyboardButton('Developers', url='https://t.me/Cybermediaworks')
     ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        InlineKeyboardButton('⭕JOIN US⭕', url='https://t.me/Cybermediaworks')
     ],[
-        InlineKeyboardButton('Help ⚙', callback_data="help")
+        InlineKeyboardButton('Close ❗', callback_data='close')
     ]]
     
     reply_markup = InlineKeyboardMarkup(buttons)
